1170d65139c6648114b1444d1859724ec28896e6
Lalatendu Bhatta <lalatendubhatta@gmail.com>
2024-10-17 13:04:44 +0530

    project config

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+
+function App() {
+  useEffect(() => {
+    fetch("http://localhost:4000")
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(console.log)
+  }, [])
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+
+const app = express()
+
+//middlewares
+app.use(express.json()) //json parser
+app.use(express.urlencoded({ extended: true })) //url data parser
+app.use(morgan("dev")) //morgan(http logger)
+app.use(cors({ origin: "http://localhost:5173" }))//cross server access
+
+//demoAPI
+app.get("/", (req, res) => res.send({ message: "SERVER AT WORK" }))
+
+//listen
+const PORT = 4000
+const hostname = "localhost"
+app.listen(PORT, hostname, () => {
+    console.log(`server running in http://${hostname}:${PORT}`);
+})
